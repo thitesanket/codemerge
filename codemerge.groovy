4e44f14266452b74cf62ab0390e73eb149f84288
thitesanketbb <thitesanket@gmail.com>
2020-06-13 14:36:38 +0530

    revert change

--- codemerge.groovy
+++ codemerge.groovy
@@ -6,5 +6,20 @@ pipeline {
                 load 'C:\\Software\\groovy\\lib\\groovy-xml-2.1.2\\groovy-xml-2.1.2.jar'
             }
         }
+        stage('Build') {
+            steps {
+                text = '''
+        <list>
+            <technology>
+            <name>Groovy</name>
+            </technology>
+        </list>
+        '''
+                list = new XmlParser().parseText(text)
+
+                assert list instanceof groovy.util.Node
+                assert list.technology.name.text() == 'Groovy'
+            }
+        }
     }
 }
