5948ab3fe59ef8c2aac22a4258ce24846edf448e
thitesanketbb <thitesanket@gmail.com>
2020-06-13 14:24:37 +0530

    Add pipeline steps

--- codemerge.groovy
+++ codemerge.groovy
@@ -1,13 +1,25 @@
-
-load 'C:\\Software\\groovy\\lib\\groovy-xml-2.1.2\\groovy-xml-2.1.2.jar'
-def text = '''
-    <list>
-        <technology>
+pipeline {
+    agent any
+    stages {
+        stage('Load') {
+            steps {
+                load 'C:\\Software\\groovy\\lib\\groovy-xml-2.1.2\\groovy-xml-2.1.2.jar'
+            }
+        }
+        stage('Build') {
+            steps {
+                def text = '''
+        <list>
+            <technology>
             <name>Groovy</name>
-        </technology>
-    </list>
-'''
-def list = new XmlParser().parseText(text)
+            </technology>
+        </list>
+        '''
+                def list = new XmlParser().parseText(text)
 
-assert list instanceof groovy.util.Node
-assert list.technology.name.text() == 'Groovy'
+                assert list instanceof groovy.util.Node
+                assert list.technology.name.text() == 'Groovy'
+            }
+        }
+    }
+}
