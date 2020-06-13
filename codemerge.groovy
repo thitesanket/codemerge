e9bc94c32f95c46d42612dbefd9247d84c55e67d
thitesanketbb <thitesanket@gmail.com>
2020-06-13 14:26:56 +0530

    Add pipeline steps

--- codemerge.groovy
+++ codemerge.groovy
@@ -8,14 +8,14 @@ pipeline {
         }
         stage('Build') {
             steps {
-                def text = '''
+                text = '''
         <list>
             <technology>
             <name>Groovy</name>
             </technology>
         </list>
         '''
-                def list = new XmlParser().parseText(text)
+                list = new XmlParser().parseText(text)
 
                 assert list instanceof groovy.util.Node
                 assert list.technology.name.text() == 'Groovy'
