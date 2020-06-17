c2194698eddc7374b8b50ed443b5f98f273188ac
thitesanketbb <thitesanket@gmail.com>
2020-06-17 19:49:57 +0530

    update xml script

--- codemerge.groovy
+++ codemerge.groovy
@@ -29,7 +29,7 @@ pipeline {
             steps {
                 script {
                     dir('src') {
-                        println "Hello World!"
+                        echo "Hello World!"
 
                         def xml1 = """<?xml version="1.0" encoding="UTF-8"?>
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
@@ -48,24 +48,8 @@ pipeline {
 </project>	
 """
                         def pom = new XmlParser().parseText(xml1)
+                        echo "Parent Version " + pom.parent.version.text()
 
-                        println "Parent Version " + pom.parent.version.text()
-                        println "Artifact Version " + pom.version.text()
-                        def atrVer =  pom.version[0]
-                        def parentVer = pom.parent.version[0]
-
-/*pom.depthFirst().find{it -> it.name() == 'version' && it.parent().name()!='parent'}.value='5.0-SNAPSHOT'*/
-                        parentVer.value  = '5.0-SNAPSHOT'
-                        atrVer .value='5.1'
-
-                        println "After replace"
-                        println "Artifact Version " + pom.version.text()
-                        println "Parent Version " + pom.parent.version.text()
-
-                        def pom1 = readFile 'pom.xml'
-                        def list = new XmlParser().parseText(pom1)
-                        def response = parser.parseText(xml)
-                        echo response.value.toString()
                     }
                 }
             }
