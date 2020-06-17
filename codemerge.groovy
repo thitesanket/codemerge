0877917687233bd79e4ea59511f560b7992334b5
thitesanketbb <thitesanket@gmail.com>
2020-06-17 19:29:02 +0530

    update xml script

--- codemerge.groovy
+++ codemerge.groovy
@@ -29,20 +29,41 @@ pipeline {
             steps {
                 script {
                     dir('src') {
-                        def xml = """
-<response version-api="2.0">
-    <value>
-        <books>
-            <book id="2">
-                <title>Don Quixote</title>
-                <author id="1">Miguel de Cervantes</author>
-            </book>
-        </books>
-    </value>
-</response>
+                        println "Hello World!"
+
+                        def xml1 = """<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+	<parent>
+		<groupId>org.springframework.boot</groupId>
+		<artifactId>spring-boot-starter-parent</artifactId>
+		<version>2.3.0.RELEASE</version>
+		<relativePath/> <!-- lookup parent from repository -->
+	</parent>
+	<groupId>com.example</groupId>
+	<artifactId>LoadRunProp</artifactId>
+	<version>0.0.1-SNAPSHOT</version>
+	<name>LoadRunProp</name>
+	<description>Demo project for Spring Boot</description>
+</project>	
 """
-                        def pom = readFile 'pom.xml'
-                        def list = new XmlParser().parseText(pom)
+                        def pom = new XmlParser().parseText(xml1)
+
+                        println "Parent Version " + pom.parent.version.text()
+                        println "Artifact Version " + pom.version.text()
+                        def atrVer =  pom.version[0]
+                        def parentVer = pom.parent.version[0]
+
+/*pom.depthFirst().find{it -> it.name() == 'version' && it.parent().name()!='parent'}.value='5.0-SNAPSHOT'*/
+                        parentVer.value  = '5.0-SNAPSHOT'
+                        atrVer .value='5.1'
+
+                        println "After replace"
+                        println "Artifact Version " + pom.version.text()
+                        println "Parent Version " + pom.parent.version.text()
+
+                        def pom1 = readFile 'pom.xml'
+                        def list = new XmlParser().parseText(pom1)
                         def response = parser.parseText(xml)
                         echo response.value.toString()
                     }
