1dc2367bab2c5da77905e1c36c71474315f4987d
thitesanketbb <thitesanket@gmail.com>
2020-06-17 21:16:13 +0530

    update xml script

--- codemerge.groovy
+++ codemerge.groovy
@@ -42,7 +42,7 @@ pipeline {
 </project>	
 """
                         def pom = new XmlParser().parseText(xml1)
-                        echo "Parent Version " + ${pom.modelVersion}
+                        echo "Parent Version " + pom
 
                     }
                 }
